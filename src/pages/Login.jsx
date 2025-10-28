c19164e9ed2a682f2a4ec9b71f52822ed7d4e26a
Lena <lena@fabios-mbp-2.lan>
2025-10-29 01:14:14 +1100

    fix the naming

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import {useNavigate} from 'react-router-dom'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Login = ({ setAuthenticate }) => {
   const navigate = useNavigate();
@@ -11,20 +11,40 @@ const Login = ({ setAuthenticate }) => {
   };
 
   return (
-    <container className='login-container'>
-      <form className='login-form' onSubmit={handleSubmit}>
-  <div className="mb-3">
-    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"  placeholder='Email address' />
-    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-  </div>
-  <div className="mb-3">
-    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-    <input type="password" className="form-control" id="exampleInputPassword1"  placeholder='Password'/>
-  </div>
-  <button type="submit" className="btn btn-danger">Submit</button>
-</form>
-</container>
-)}
+    <container className="login-container">
+      <form className="login-form" onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            placeholder="Email address"
+          />
+          <div id="emailHelp" className="form-text">
+            We'll never share your email with anyone else.
+          </div>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+            placeholder="Password"
+          />
+        </div>
+        <button type="submit" className="btn btn-danger">
+          Login
+        </button>
+      </form>
+    </container>
+  );
+};
 
-export default Login
+export default Login;
